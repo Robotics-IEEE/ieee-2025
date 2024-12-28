3a24b498dce8d8f13595f54d61975fda63a8ed18
robodawg-cv-uga <robodawg.cv.uga@gmail.com>
2024-12-28 16:33:47 -0500

    KK added code for shifter(pubsub)

--- src/driver.py
+++ src/driver.py
@@ -9,28 +9,28 @@ class Driver:
 
     def goal_position_publish(self):
         goal_pos_publish  = rospy.Publisher('goal_position', "point", queue_size=10)
-        rospy.init_node('driver', anonymous=False)
+        rospy.init_node('driver_publish', anonymous=False)
         while not rospy.is_shutdown():
             goal_pos_publish.publish("Output here")
             self.rate.sleep()
         
     def intake_control_publish():
         intake_control_publish  = rospy.Publisher('intake_control', "bool", queue_size=10)
-        rospy.init_node('driver', anonymous=False)
+        rospy.init_node('driver_publish', anonymous=False)
         while not rospy.is_shutdown():
             intake_control_publish.publish("Output here")
             self.rate.sleep()
 
     def outtake_control_publish():
         outtake_control_publish  = rospy.Publisher('outtake_control', "bool", queue_size=10)
-        rospy.init_node('driver', anonymous=False)
+        rospy.init_node('driver_publish', anonymous=False)
         while not rospy.is_shutdown():
             outtake_control_publish.publish("Output here")
             self.rate.sleep()
 
     def shifter_publish():
         shifter_publish  = rospy.Publisher('shifter', "bool", queue_size=10)
-        rospy.init_node('driver', anonymous=False)
+        rospy.init_node('driver_publish', anonymous=False)
         while not rospy.is_shutdown():
             shifter_publish.publish("Output here")
             self.rate.sleep()
---
+++ src/shifter.py
@@ -0,0 +1,22 @@
+import rospy
+
+
+class Shifter():
+    rate = rospy.Rate(10)
+    def __init__():
+        pass
+    
+    def shifter_status_publish():
+        rospy.init_node("shifter publish", anonymous=False)
+
+        shifter_publish = rospy.Publisher("shifter_status", "status_type", queue_size=10)
+
+        while not rospy.is_shutdown():
+
+            self.rate.sleep()
+
+    def shifter_control_subscribe():
+        rospy.init_node("driver_publish", anonymous=False)
+        rospy.Subscriber("shifter_control", "callback?")
+
+        rospy.spin()
