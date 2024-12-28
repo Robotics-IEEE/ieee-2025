769dd58729459bd2967216ff24577c36539278df
robodawg-cv-uga <robodawg.cv.uga@gmail.com>
2024-12-28 15:54:16 -0500

    KK added some structure to the driver node

--- src/driver.py
+++ src/driver.py
@@ -1,19 +1,55 @@
+import rospy
 
-
+#TODO: change "point" to a real data type
+#TODO: make the publishers publish a legitimate output
 class Driver:
+    rate = rospy.Rate(10)
     def __init__():
         pass
 
-    def goal_position_publish():
-        pass
-
+    def goal_position_publish(self):
+        goal_pos_publish  = rospy.Publisher('goal_position', "point", queue_size=10)
+        rospy.init_node('driver', anonymous=False)
+        while not rospy.is_shutdown():
+            goal_pos_publish.publish("Output here")
+            self.rate.sleep()
+        
     def intake_control_publish():
-        pass
+        intake_control_publish  = rospy.Publisher('intake_control', "bool" , queue_size=10)
+        rospy.init_node('driver', anonymous=False)
+        while not rospy.is_shutdown():
+            goal_pos_publish.publish("Output here")
+            self.rate.sleep()
 
     def outtake_control_publish():
-        pass
+        goal_pos_publish  = rospy.Publisher('goal_position', "point", queue_size=10)
+        rospy.init_node('driver', anonymous=False)
+        while not rospy.is_shutdown():
+            goal_pos_publish.publish("Output here")
+            self.rate.sleep()
 
     def shifter_publish():
+        goal_pos_publish  = rospy.Publisher('goal_position', "point", queue_size=10)
+        rospy.init_node('driver', anonymous=False)
+        while not rospy.is_shutdown():
+            goal_pos_publish.publish("Output here")
+            self.rate.sleep()
+
+    def goal_status_subscribe():
+        pass
+    
+    def magnet_subscribe():
         pass
 
+    def shifter_status_subscribe():
+        pass
+
+    def vision_status_subscribe():
+        pass
+
+
+
+
+
+    
     
