1670fa025dd90cb65ef8c347efc1fe8c7397c98a
Rishab Seshadri <rishab.seshadri@gmail.com>
2024-12-28 16:53:32 -0500

    RS Added TODO for tomorrow shifter

--- src/shifter.py
+++ src/shifter.py
@@ -1,22 +1,33 @@
 import rospy
 
+# TODO: Create status_type
 
-class Shifter():
-    rate = rospy.Rate(10)
-    def __init__():
-        pass
-    
-    def shifter_status_publish():
-        rospy.init_node("shifter publish", anonymous=False)
+class Shifter():    
+    def __init__(self):
+        # Initialize the node
+        try:
+            self.rate = rospy.Rate(10) # Hz
+            rospy.init_node("shifter", anonymous=False)
+        except Exception as e:
+            rospy.logerr(f"Failed to initialize shifter: {e}")
+            raise
 
-        shifter_publish = rospy.Publisher("shifter_status", "status_type", queue_size=10)
+    def shifter_status_publish(self):
+        shifter_publish = rospy.Publisher("shifter_status", status_type, queue_size=10)
 
         while not rospy.is_shutdown():
+            try:
+                # TODO: Build status type
+                # TODO: Fix dynamixel READ code here
+                print("hi")
+            except Exception as e:
+                rospy.logerr(f"Error reading data: {e}")
 
+            # Sleep for rate of 10Hz
             self.rate.sleep()
 
-    def shifter_control_subscribe():
-        rospy.init_node("driver_publish", anonymous=False)
+    def shifter_control_subscribe(self):
         rospy.Subscriber("shifter_control", "callback?")
-
+        # TODO: Fix dynamixel WRITE code here
+        # TODO: Make callback
         rospy.spin()
