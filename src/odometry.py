58dda6d18b28a3b57f0f2f647923db20abe23b6e
Rishab Seshadri <rishab.seshadri@gmail.com>
2025-01-21 15:46:11 -0500

    RS adding dependency for twist

--- src/odometry.py
+++ src/odometry.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
+
 import rospy
+from geometry_msgs.msg import Twist
 
 class Odometry():
 
     def __init__(self):
-        rospy.init_node("odometry", anonymous=False)
-        rate = rospy.Rate(10)
+
+        try:
+            rospy.init_node("odometry", anonymous=False)
+            self.rate = rospy.Rate(10)
+
+        except Exception as e:
+            raise
 
     def odometry_publish(self):
-        shifter_publish = rospy.Publisher("odometry_publish", "point", queue_size=10)
+        shifter_publish = rospy.Publisher("odometry_publish", Twist, queue_size=10)
+
         while not rospy.is_shutdown():
             self.rate.sleep()
