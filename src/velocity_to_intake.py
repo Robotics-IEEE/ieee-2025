4e6285c4d1b276d479bdd02e915666d3f6151efe
Rishab Seshadri <rishab.seshadri@gmail.com>
2025-03-26 20:52:27 -0400

    intake node written

--- src/velocity_to_intake.py
+++ src/velocity_to_intake.py
@@ -7,12 +7,12 @@ class VelocityToIntake():
 
     def __init__(self):
         rospy.init_node("velocity_to_intake", anonymous=False)
-        self.rate = rospy.Rate(10)
         # TODO: fix this id 
         self.intake_motor = Motor(id=4, drivetrain=False) 
 
         # TODO: fix the speed
         self.speed = 1000
+        rospy.Subscriber("intake_control", Bool, self.intake_callback)
 
     def intake_callback(self, msg):
         if msg.data:
@@ -21,14 +21,12 @@ class VelocityToIntake():
         else:
             self.intake_motor.stop()
 
-    def intake_control_subscribe(self):
-        rospy.Subscriber("intake_control", Bool, self.intake_callback)
-        rospy.spin()
 
 # Execute velocity to intake
 if __name__ == "__main__":
     try:
         velocity_to_intake = VelocityToIntake()
-        # Run the publishers and subscribers here
+        rospy.spin()
+
     except rospy.ROSInterruptException:
         pass
